9c14ea6274738aad8906a55532538118cb49f2ff
Burak <ahmetcanburak54@gmail.com>
2026-01-31 18:46:23 +0300

    Fix syntax error: Correct brace placement int main function

--- cp/sys_cp_v2.c
+++ cp/sys_cp_v2.c
@@ -3,47 +3,61 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+
 #define BUFFER_SIZE 4096
-int main(int argc, char *argv[]){
-int inputFd, outputFd;
-ssize_t numRead;
-char buffer[BUFFER_SIZE];
-struct stat sourceMetadata;
-if (argc != 3){
-	printf("Usage: %s <source> <destination>", argv[0]);
-	exit(EXIT_FAILURE);
-}
 
-inputFd = open(argv[1],O_RDONLY);
-if(inputFd == -1){
-perror("Error: source file couldn't opened");
-exit(EXIT_FAILURE);
-}
+int main(int argc, char *argv[]) {
+    int inputFd, outputFd;
+    ssize_t numRead;
+    char buffer[BUFFER_SIZE];
+    struct stat sourceMetadata;
 
-if(fstat(inputFd,&sourceMetadata) == -1){
-	perror("Error: file attributes couldn't read");
-exit(EXIT_FAILURE);
-}
-outputFd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-if(outputFd == -1){
-perror("Error: destination file couldn't be created");
-exit(EXIT_FAILURE);
-}
-while((numRead = read(inputFd, buffer, BUFFER_SIZE)) > 0){
-if(write(outputFd,buffer,numRead) != numRead){
-perror("Error: writing error");
-close(inputFd);
-close(outputFd);
-exit(EXIT_FAILURE);
-}
-}
-#ifndef _WIN32
-if(fchmod(outputFd, sourceMetadata.st_mode) == -1){
-perror("Error: permissions couldn't edited");
-#endif
-}
-close(inputFd);
-close(outputFd);
-printf("Copying and permissions transfer is code\n");
-return 0;
+    if (argc != 3) {
+        printf("Usage: %s <source> <destination>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /* 1. Kaynak Dosyayı Aç */
+    inputFd = open(argv[1], O_RDONLY);
+    if (inputFd == -1) {
+        perror("Error: source file couldn't opened");
+        exit(EXIT_FAILURE);
+    }
+
+    /* 2. Özellikleri Oku */
+    if (fstat(inputFd, &sourceMetadata) == -1) {
+        perror("Error: file attributes couldn't read");
+        exit(EXIT_FAILURE);
+    }
+
+    /* 3. Hedef Dosyayı Aç */
+    outputFd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (outputFd == -1) {
+        perror("Error: destination file couldn't be created");
+        close(inputFd);
+        exit(EXIT_FAILURE);
+    }
+
+    /* 4. Kopyala */
+    while ((numRead = read(inputFd, buffer, BUFFER_SIZE)) > 0) {
+        if (write(outputFd, buffer, numRead) != numRead) {
+            perror("Error: writing error");
+            close(inputFd);
+            close(outputFd);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    /* 5. İzinleri Kopyala (Sadece Linux/Mac - Windows hariç) */
+    #ifndef _WIN32
+        if (fchmod(outputFd, sourceMetadata.st_mode) == -1) {
+            perror("Error: permissions couldn't edited");
+        }
+    #endif
+
+    close(inputFd);
+    close(outputFd);
+
+    printf("Copying and permissions transfer is complete.\n");
+    return 0;
 }
